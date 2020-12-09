24364f1be1e1f922d071d8954f8469718679842b
Andrey <Hohlov908@gmail.com>
2020-12-10 00:10:21 +0300

    Add Kasimov corrections, fix name button users

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -65,22 +65,20 @@
             this.flowLayoutPanel1.AutoSize = true;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 626);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 501);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(802, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(642, 0);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // changeRoleUserButton
             // 
             this.changeRoleUserButton.AutoSize = true;
             this.changeRoleUserButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.changeRoleUserButton.Location = new System.Drawing.Point(193, 4);
-            this.changeRoleUserButton.Margin = new System.Windows.Forms.Padding(4);
+            this.changeRoleUserButton.Location = new System.Drawing.Point(154, 3);
             this.changeRoleUserButton.Name = "changeRoleUserButton";
-            this.changeRoleUserButton.Size = new System.Drawing.Size(189, 34);
+            this.changeRoleUserButton.Size = new System.Drawing.Size(151, 27);
             this.changeRoleUserButton.TabIndex = 0;
-            this.changeRoleUserButton.Text = "Плдьзователи";
+            this.changeRoleUserButton.Text = "Пользователи";
             this.changeRoleUserButton.UseVisualStyleBackColor = true;
             this.changeRoleUserButton.Click += new System.EventHandler(this.changeRoleUser_Click);
             // 
@@ -88,7 +86,6 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -100,16 +97,14 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel6);
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainer1.Size = new System.Drawing.Size(802, 626);
-            this.splitContainer1.SplitterDistance = 572;
-            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.Size = new System.Drawing.Size(642, 501);
+            this.splitContainer1.SplitterDistance = 457;
             this.splitContainer1.TabIndex = 1;
             // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer2.Location = new System.Drawing.Point(0, 50);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer2.Location = new System.Drawing.Point(0, 39);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -120,9 +115,8 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.tableLayoutPanel4);
-            this.splitContainer2.Size = new System.Drawing.Size(572, 576);
-            this.splitContainer2.SplitterDistance = 530;
-            this.splitContainer2.SplitterWidth = 5;
+            this.splitContainer2.Size = new System.Drawing.Size(457, 462);
+            this.splitContainer2.SplitterDistance = 425;
             this.splitContainer2.TabIndex = 5;
             // 
             // dataGridView1
@@ -132,10 +126,9 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(572, 530);
+            this.dataGridView1.Size = new System.Drawing.Size(457, 425);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
@@ -147,28 +140,26 @@
             this.tableLayoutPanel4.ColumnCount = 3;
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.75F));
             this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.25F));
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 204F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 163F));
             this.tableLayoutPanel4.Controls.Add(this.updateTableButton, 2, 0);
             this.tableLayoutPanel4.Controls.Add(this.insertRowButton, 1, 0);
             this.tableLayoutPanel4.Controls.Add(this.deleteSelectedRowButton, 0, 0);
             this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel4.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel4.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel4.Name = "tableLayoutPanel4";
             this.tableLayoutPanel4.RowCount = 1;
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(572, 41);
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(457, 33);
             this.tableLayoutPanel4.TabIndex = 2;
             // 
             // updateTableButton
             // 
             this.updateTableButton.AutoSize = true;
             this.updateTableButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.updateTableButton.Location = new System.Drawing.Point(372, 4);
-            this.updateTableButton.Margin = new System.Windows.Forms.Padding(4);
+            this.updateTableButton.Location = new System.Drawing.Point(296, 3);
             this.updateTableButton.Name = "updateTableButton";
-            this.updateTableButton.Size = new System.Drawing.Size(196, 33);
+            this.updateTableButton.Size = new System.Drawing.Size(158, 27);
             this.updateTableButton.TabIndex = 0;
             this.updateTableButton.Text = "Обновление";
             this.updateTableButton.UseVisualStyleBackColor = true;
@@ -178,10 +169,9 @@
             // 
             this.insertRowButton.AutoSize = true;
             this.insertRowButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.insertRowButton.Location = new System.Drawing.Point(165, 4);
-            this.insertRowButton.Margin = new System.Windows.Forms.Padding(4);
+            this.insertRowButton.Location = new System.Drawing.Point(131, 3);
             this.insertRowButton.Name = "insertRowButton";
-            this.insertRowButton.Size = new System.Drawing.Size(199, 33);
+            this.insertRowButton.Size = new System.Drawing.Size(159, 27);
             this.insertRowButton.TabIndex = 1;
             this.insertRowButton.Text = "Добавление";
             this.insertRowButton.UseVisualStyleBackColor = true;
@@ -191,10 +181,9 @@
             // 
             this.deleteSelectedRowButton.AutoSize = true;
             this.deleteSelectedRowButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.deleteSelectedRowButton.Location = new System.Drawing.Point(4, 4);
-            this.deleteSelectedRowButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteSelectedRowButton.Location = new System.Drawing.Point(3, 3);
             this.deleteSelectedRowButton.Name = "deleteSelectedRowButton";
-            this.deleteSelectedRowButton.Size = new System.Drawing.Size(153, 33);
+            this.deleteSelectedRowButton.Size = new System.Drawing.Size(122, 27);
             this.deleteSelectedRowButton.TabIndex = 0;
             this.deleteSelectedRowButton.Text = "Удаление";
             this.deleteSelectedRowButton.UseVisualStyleBackColor = true;
@@ -210,11 +199,10 @@
             this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel1, 1, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(572, 50);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(457, 39);
             this.tableLayoutPanel3.TabIndex = 4;
             // 
             // tableLayoutPanel2
@@ -223,28 +211,26 @@
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.07975F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.92025F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel2.Controls.Add(this.loginButton, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.changeRoleUserButton, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 4);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(386, 42);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(308, 33);
             this.tableLayoutPanel2.TabIndex = 3;
             // 
             // loginButton
             // 
             this.loginButton.AutoSize = true;
             this.loginButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.loginButton.Location = new System.Drawing.Point(4, 4);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(4);
+            this.loginButton.Location = new System.Drawing.Point(3, 3);
             this.loginButton.Name = "loginButton";
             this.loginButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.loginButton.Size = new System.Drawing.Size(181, 34);
+            this.loginButton.Size = new System.Drawing.Size(145, 27);
             this.loginButton.TabIndex = 0;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = true;
@@ -258,23 +244,21 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.namesTablesComboBox, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(398, 4);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(317, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(170, 42);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(137, 33);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // namesTablesComboBox
             // 
             this.namesTablesComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.namesTablesComboBox.FormattingEnabled = true;
-            this.namesTablesComboBox.Location = new System.Drawing.Point(4, 4);
-            this.namesTablesComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.namesTablesComboBox.Location = new System.Drawing.Point(3, 3);
             this.namesTablesComboBox.Name = "namesTablesComboBox";
-            this.namesTablesComboBox.Size = new System.Drawing.Size(162, 24);
+            this.namesTablesComboBox.Size = new System.Drawing.Size(131, 21);
             this.namesTablesComboBox.TabIndex = 1;
             this.namesTablesComboBox.SelectedIndexChanged += new System.EventHandler(this.namesTablesComboBox_SelectedIndexChanged);
             // 
@@ -287,33 +271,30 @@
             this.tableLayoutPanel6.Controls.Add(this.consoleLogTextBox, 0, 0);
             this.tableLayoutPanel6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel6.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel6.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel6.Name = "tableLayoutPanel6";
             this.tableLayoutPanel6.RowCount = 1;
             this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel6.Size = new System.Drawing.Size(225, 626);
+            this.tableLayoutPanel6.Size = new System.Drawing.Size(181, 501);
             this.tableLayoutPanel6.TabIndex = 0;
             // 
             // consoleLogTextBox
             // 
             this.consoleLogTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.consoleLogTextBox.Location = new System.Drawing.Point(4, 4);
-            this.consoleLogTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.consoleLogTextBox.Location = new System.Drawing.Point(3, 3);
             this.consoleLogTextBox.Multiline = true;
             this.consoleLogTextBox.Name = "consoleLogTextBox";
             this.consoleLogTextBox.ReadOnly = true;
-            this.consoleLogTextBox.Size = new System.Drawing.Size(217, 618);
+            this.consoleLogTextBox.Size = new System.Drawing.Size(175, 495);
             this.consoleLogTextBox.TabIndex = 1;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(802, 626);
+            this.ClientSize = new System.Drawing.Size(642, 501);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "Турфирмы";
             this.Load += new System.EventHandler(this.MainForm_Load);
--- MyConsole/MyConsole.cs
+++ MyConsole/MyConsole.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Lab7_Bd_Mk2_Entity.MyConsole
 {
     public class MyConsole
     {
         TextBox outputTextBox;
+        const bool StipidRev = true;// you
 
         public MyConsole(ref TextBox aoutputTextBox)
         {
@@ -37,7 +39,50 @@ namespace Lab7_Bd_Mk2_Entity.MyConsole
         {
             Console.Error.WriteLine(message);
             outputTextBox.Text = "";
-            outputTextBox.Text += (message + Environment.NewLine);
+            if (StipidRev)
+            {
+                string s = message;
+                Regex regex = new Regex("Запрещено разрешение");
+                MatchCollection matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    outputTextBox.Text += ("Нет прав на редактирование");
+                }
+
+                regex = new Regex("Не удается вставить повторяющийся ключ в объект");
+                matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    outputTextBox.Text += ("Данный ID уже занят");
+                }
+
+                regex = new Regex("Ошибка преобразования");
+                matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    outputTextBox.Text += ("Ожидалось значение в верном формате");
+                }
+
+                regex = new Regex("переполнению");
+                matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    outputTextBox.Text += ("Значение было переполнено");
+                }
+
+                regex = new Regex("Конфликт инструкции");
+                matches = regex.Matches(s);
+                if (matches.Count > 0)
+                {
+                    outputTextBox.Text += ("Действие невозможно из-за конфликтов с другими таблицами");
+                }
+            }
+            else
+            {
+                outputTextBox.Text += (message);
+            }
+
+            outputTextBox.Text += Environment.NewLine;
         }
     }
 }
